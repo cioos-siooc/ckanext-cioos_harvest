9b9be6da390dbf985ca3ae782aa4e80dc79e3f4d
Matthew Foster <matthew.foster@hakai.org>
2021-02-18 00:17:58 -0800

    extend harvesters
    fix cioos-siooc/ckan#63
    
    This creates a custom ckan harvester which indexes external xml content and provides better error messages in the harvest log then before. XML indexing has been removed from the default ckan harvester in favour of this approach.
    
    To use the harvester add 'ckan_cioos_harvester' to your plugins list in your production.ini

--- ckanext/cioos_harvest/plugin.py
+++ ckanext/cioos_harvest/plugin.py
@@ -1,16 +1,109 @@
 import ckan.plugins as plugins
+from ckan import model
 import ckan.plugins.toolkit as toolkit
 from ckanext.spatial.interfaces import ISpatialHarvester
 from ckanext.spatial.validation.validation import BaseValidator
+from ckanext.harvest.interfaces import IHarvester
+from ckanext.harvest.model import HarvestObjectError
+from ckanext.harvest.harvesters.ckanharvester import CKANHarvester
 import ckan.lib.munge as munge
 import json
 import logging
-import subprocess
-import os
-import numbers
 from numbers import Number
+import urllib2
+import socket
+import xml.etree.ElementTree as ET
 
 log = logging.getLogger(__name__)
+
+
+def load_json(j):
+    try:
+        new_val = json.loads(j)
+    except Exception:
+        new_val = j
+    return new_val
+
+def _get_xml_url_content(xml_url, urlopen_timeout, harvest_object):
+    try:
+        xml_str = urllib2.urlopen(xml_url, timeout=urlopen_timeout).read(100000)  # read only 100 000 chars
+        ET.XML(xml_str)  # test for valid xml
+        return xml_str
+    except ET.ParseError as e:
+        msg = '%s: %s. From external XML content at %s' % (type(e).__name__, str(e), xml_url)
+        log.warn(msg)
+        err = HarvestObjectError(message=msg, object=harvest_object, stage='Import')
+        err.save()
+    except urllib2.URLError as e:
+        msg = '%s: %s. From external XML content at %s' % (type(e).__name__, str(e), xml_url)
+        log.warn(msg)
+        err = HarvestObjectError(message=msg, object=harvest_object, stage='Import')
+        err.save()
+    except socket.timeout as e:
+        msg = '%s: %s. From external XML content at %s' % (type(e).__name__, str(e), xml_url)
+        log.warn(msg)
+        err = HarvestObjectError(message=msg, object=harvest_object, stage='Import')
+        err.save()
+    except Exception as e:
+        msg = '%s: %s. From external XML content at %s' % (type(e).__name__, str(e), xml_url)
+        log.warn(msg)
+        err = HarvestObjectError(message=msg, object=harvest_object, stage='Import')
+        err.save()
+    finally:
+        return None
+
+def _extract_xml_from_harvest_object(context, package_dict, harvest_object):
+    content = harvest_object.content
+    if content.startswith('<'):
+        package_dict['extras_harvest_document_content'] = harvest_object.content
+    else:
+        log.warn('Unable to find harvest object "%s" '
+                 'referenced by dataset "%s". Trying xml url',
+                 harvest_object.id, package_dict['id'])
+
+        # try reading from xml url
+        xml_str = ''
+        xml_url = load_json(package_dict.get('xml_location_url'))
+        urlopen_timeout = float(toolkit.config.get('ckan.index_xml_url_read_timeout', '500')) / 1000.0  # get value in millieseconds but urllib assumes it is in seconds
+
+        # single file
+        if xml_url and isinstance(xml_url, basestring):
+            package_dict['extras_harvest_document_content'] = _get_xml_url_content(xml_url, urlopen_timeout, harvest_object)
+
+        # list of files
+        if xml_url and isinstance(xml_url, list):
+            for xml_file in xml_url:
+                xml_str = xml_str + '<doc>' + _get_xml_url_content(xml_url, urlopen_timeout, harvest_object) + '</doc>'
+
+            if xml_str:
+                xml_str = '<?xml version="1.0" encoding="utf-8"?><docs>' + xml_str + '</docs>'
+                package_dict['extras_harvest_document_content'] = xml_str
+
+    return package_dict
+
+
+
+class CIOOSCKANHarvester(CKANHarvester):
+
+    def info(self):
+        return {
+            'name': 'ckan_cioos',
+            'title': 'CKAN CIOOS',
+            'description': 'Harvests remote CKAN instances with improved handling/indexing of external xml files',
+            'form_config_interface': 'Text'
+        }
+
+    def modify_package_dict(self, package_dict, harvest_object):
+
+        context = {
+            'model': model,
+            'session': model.Session,
+            'user': toolkit.c.user
+        }
+
+        package_dict = _extract_xml_from_harvest_object(context, package_dict, harvest_object)
+
+        return package_dict
 
 
 # place holder, spatial extension expects a validator to be present
@@ -72,26 +165,6 @@ class Cioos_HarvestPlugin(plugins.SingletonPlugin):
         except Exception:
             new_val = val
         return new_val
-
-    # def transform_to_iso(self, original_document, original_format, harvest_object):
-    #     log.debug('original_format:%r',original_format)
-    #     return original_document
-    #
-    #     lowered = original_document.lower()
-    #     if '</mdb:MD_Metadata>'.lower() in lowered:
-    #         log.debug('Found ISO19115-3 format, transforming to ISO19139')
-    #
-    #         xsl_filename = os.path.abspath("./ckanext-spatial/ckanext/spatial/transformers/ISO19115-3/toISO19139.xsl")
-    #         process = subprocess.Popen(["saxonb-xslt", "-s:-", xsl_filename], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #         process.stdin.write(original_document.encode('utf-8'))
-    #         newDoc, errors = process.communicate()
-    #         process.stdin.close()
-    #         if errors:
-    #             log.error(errors)
-    #             return None
-    #         return newDoc
-    #
-    #     return None
 
     def _get_object_extra(self, harvest_object, key):
         '''
@@ -161,9 +234,13 @@ class Cioos_HarvestPlugin(plugins.SingletonPlugin):
     def get_package_dict(self, context, data_dict):
         package_dict = data_dict['package_dict']
         iso_values = data_dict['iso_values']
+        harvest_object = data_dict['harvest_object']
         source_config = json.loads(data_dict['harvest_object'].source.config)
         xml_location_url = self._get_object_extra(data_dict['harvest_object'], 'waf_location')
         xml_modified_date = self._get_object_extra(data_dict['harvest_object'], 'waf_modified_date')
+
+        # load remote xml content
+        package_dict = _extract_xml_from_harvest_object(context, package_dict, harvest_object)
 
         # Handle Scheming, Composit, and Fluent extensions
         loaded_plugins = plugins.toolkit.config.get("ckan.plugins")
